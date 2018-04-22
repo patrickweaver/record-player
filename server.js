85d92b5d96803d4c8421a4da57dad467d71c9ad3
Glitch (record-collection) <none>
2018-04-22 20:20:22 +0000

    🚶💿 Checkpoint ./server.js:217543/30

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ app.use(express.static('public'));
 
 app.use((req, res, next) => {
   console.log(req);
-  console.log("Hello");
+  console.log('Hello');
   next();
 });
 
-app.get("/", (req, res) => {
-  res.sendFile('/app/views/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
