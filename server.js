5caa5fb3b60f93e3097b1f4abf642189bc86f18e
Glitch (record-collection) <none>
2018-04-22 23:37:21 +0000

    🍪👋 Checkpoint ./server.js:217543/384 ./package.json:217543/1437 ./views/index.html:217543/225

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var multer  = require('multer')
+var upload = multer({ dest: __dirname + '/uploads/' })
+
+const GCP_API_KEY = process.env.GCP_API_KEY;
 
 app.use(express.static('public'));
 
@@ -12,6 +16,13 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.post('/', upload.single('cover'), (req, res) => {
+  
+  
+  
+});
+
 
 
 var listener = app.listen(process.env.PORT, function () {
