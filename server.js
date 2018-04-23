78aec9999e0d31749de23226a083ac4f631e0746
Glitch (record-collection) <none>
2018-04-23 21:34:08 +0000

    🐺🚞 Checkpoint ./package.json:217543/1774 ./server.js:217543/1987 ./.gitignore:217543/929

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var app = express();
 var multer  = require('multer');
-var upload = multer({ dest: __dirname + '/public/' })
+var upload = multer({ dest: __dirname + '/public/images/' })
+var rp = require('request-promise-native');
 
 const GCP_API_KEY = process.env.GCP_API_KEY;
+
+function postGcpVision(imagePath) {
+  var options = {
+    method: 'POST',
+    uri: 'https://vision.googleapis.com/v1/images:annotate?key=' + GCP_API_KEY,
+    body: {
+      "requests":[
+        {
+          "image":{
+            "content":"imagePath"
+          },
+          "features":[
+            {
+              "type":"LABEL_DETECTION",
+              "maxResults":1
+            }
+          ]
+        }
+      ]
+    },
+    json: true // Automatically stringifies the body to JSON
+  };
+ 
+  rp(options)
+  .then(function (parsedBody) {
+      // POST succeeded...
+  })
+  .catch(function (err) {
+      // POST failed...
+  });
+  
+}
+
+
+
+
 
 app.use(express.static('public'));
 
@@ -19,7 +56,7 @@ app.get('/', (req, res) => {
 
 app.post('/', upload.single('file'), function(req, res) {
   //res.send(req.file);
-  res.send("<img src='/" + req.file.filename + "'>");
+  res.send("<img src='/images/" + req.file.filename + "'>");
 });
 
 
