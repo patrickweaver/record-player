8cf47f720dc52a558106d8094b62d73c03984ca8
Glitch (record-collection) <none>
2018-04-24 02:05:55 +0000

    👛🏹 Checkpoint ./server.js:217543/551

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var app = express();
 var multer  = require('multer');
 var upload = multer({ dest: __dirname + '/public/images/' })
 var rp = require('request-promise-native');
+const querystring = require('querystring');
 
 const gcpApiUrl = 'https://vision.googleapis.com/v1/images:annotate?'
 const GCP_API_KEY = process.env.GCP_API_KEY;
 
 const projectUrl = 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me';
+const redirectPath = '/b';
 
 const spotifyApiUrl = '	https://api.spotify.com/v1/';
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
 function postGcpVision(imagePath, req, res) {
   let gcpVisionOptions = {
@@ -98,8 +102,15 @@ app.post('/', upload.single('file'), function(req, res) {
   postGcpVision(imagePath, req, res);
 });
 
-app.get('/env', (req, res) => {
-  res.send(process.env.PROJECT_DOMAIN);
+app.get('/a', (req, res) => {
+  let query = {
+    client_id: SPOTIFY_CLIENT_ID,
+    response: "token",
+    redirect_uri
+  }
+  
+  
+  //res.redirect("https://accounts.spotify.com/authorize?client_id=" + SPOTIFY_CLIENT_ID + "&response_type=token&
 });
 
 
