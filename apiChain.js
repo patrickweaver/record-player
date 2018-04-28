cd8f53c65e893f0ef616bbc9a43d46f05d8e61dd
Glitch (record-player) <none>
2018-04-28 23:03:28 +0000

    ⛽️🏯 Checkpoint ./apiChain.js:217543/2633

--- apiChain.js
+++ apiChain.js
@@ -5,6 +5,46 @@ const googleVision = require('./googleVision');
 const spotify = require('./spotify');
 const censoredWords = require('./censoredWords');
 
+
+function checkGoogleVisionGuess(gvGuess) {
+  console.log(JSON.stringify(gvGuess));
+  let guess = gvGuess.responses[0].webDetection.bestGuessLabels[0].label;
+  console.log("guess: " + guess);
+  let guessArray = guess.split(" ");
+  let safeArray = []
+  console.log("guessArray: ");
+  console.log(guessArray);
+  for (var i in guessArray) {
+    let safe = true;
+    if (censoredWords.censoredWords.indexOf(guessArray[i]) > -1) {
+      safe = false; 
+    }
+    if (safe) {
+      safeArray.push(guessArray[i]); 
+    } else {
+      // Need to add these to a DB
+      console.log("NOT SAFE");
+      console.log(guessArray[i]);
+    }
+  }
+  console.log("safeArray: ");
+  console.log(safeArray);  
+  return safeArray.join(" ");   
+}
+
+async function askSpotifyApi(safeGuess) {  
+  let spotifyQueryOptions = spotify.queryOptions(spotify.token, safeGuess);
+
+  let url = await rp(spotifyQueryOptions)
+  .then(checkSpotifyData)
+  .catch(function(err) {
+    console.log("SpotifyError");
+    throw(err);
+  });
+  console.log("url: " + url);
+  return url;
+}
+
 function checkSpotifyData(spotifyData) {
   console.log("spotifyData: ");
   console.log(JSON.stringify(spotifyData));
@@ -17,56 +57,17 @@ function checkSpotifyData(spotifyData) {
 
 
 module.exports = async function(imagePath, req, res) {
-  var guess = "";
   let gcpVisionOptions = googleVision.getGcpOptions(projectUrl + imagePath);
   
-  let url = await rp(gcpVisionOptions)
-  .then(function (parsedBody) {
-    console.log(JSON.stringify(parsedBody));
-    guess = parsedBody.responses[0].webDetection.bestGuessLabels[0].label;
-    console.log("guess: " + guess);
-    let guessArray = guess.split(" ");
-    let safeArray = []
-    console.log("guessArray: ");
-    console.log(guessArray);
-    for (var i in guessArray) {
-      let safe = true;
-      if (censoredWords.censoredWords.indexOf(guessArray[i]) > -1) {
-        safe = false; 
-      }
-      if (safe) {
-        safeArray.push(guessArray[i]); 
-      } else {
-        // Need to add these to a DB
-        console.log("NOT SAFE");
-        console.log(guessArray[i]);
-      }
-    }
-    console.log("safeArray: ");
-    console.log(safeArray);  
-    return safeArray.join(" ");
-    
-  })
-  .then(async function (safeGuess) {
-    
-    let spotifyQueryOptions = spotify.queryOptions(spotify.token, safeGuess);
-    
-    let url = await rp(spotifyQueryOptions)
-    .then(checkSpotifyData)
-    .catch(function(err) {
-      console.log("SpotifyError");
-      throw(err);
-    });
-    console.log("url: " + url);
-    return url;
-  })
-  .then(function(url) {
+  let apiRes = await rp(gcpVisionOptions)
+  .then(checkGoogleVisionGuess)
+  .then(askSpotifyApi)
+  .then((url) => {
     return {error: false, url: url};
   })
   .catch(function (err) {
     console.log("GCP Error");
     console.log(err);
-    res.send(err);
     return {error: true, errorMessage: err};
   });
   
