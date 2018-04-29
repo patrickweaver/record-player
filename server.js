6b33c4dcb74c437819377ca9958589a497b2afdc
Glitch (record-player) <none>
2018-04-29 19:25:20 +0000

    🏤🐆 Checkpoint ./views/auth.hbs:217543/60 ./server.js:217543/2894 ./package.json:217543/1055 ./views/layout.hbs:217543/55

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser());
+const hbs = require("hbs");
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+
 var multer  = require('multer');
-var upload = multer({ dest: __dirname + '/public/images/' })
+var upload = multer({ dest: __dirname + '/public/images/' });
 var rp = require('request-promise-native');
 const querystring = require('querystring');
 const url = require('url');
@@ -10,45 +17,9 @@ const projectUrl = 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me';
 const apiChain = require('./apiChain');
 const spotify = require('./spotify');
 
-const hbs = require("hbs");
-app.set("view engine", "hbs");
-app.set("views", "views");
+
 
 /* Routes */
-
-app.use(express.static('public'));
-
-
-app.get('/', (req, res) => {
-  res.redirect('/auth');
-});
-
-
-app.get('/camera', (req, res) => {
-  res.render('camera', {});
-});
-
-app.post('/player', upload.single('file'), async function(req, res) {
-  let imagePath = '/images/' + req.file.filename;
-  let apiResponse = await apiChain(imagePath, req, res);
-  // {error: bool, url: url, errorMessage: errorMessage}
-  if (!apiResponse.error) {
-    res.render('player', {
-      embed: spotify.embed[0] + apiResponse.albumId + spotify.embed[1] 
-    });
-  } else {
-    res.send("Error: " + apiResponse.errorMessage);
-  }
-  try {
-    fs.unlinkSync('/app/public' + imagePath);
-  } catch (err) {
-    console.log('error deleting ' + imagePath + ': ' + err);
-  }
-});
-
-app.get('/player', function(req,res) {
-  res.redirect('/camera');
-});
 
 app.get('/auth', (req, res) => {
   let query = spotify.authQueryStringObject;
@@ -74,6 +45,8 @@ app.get('/auth-callback', (req, res) => {
       console.log("refresh_token: " + data.refresh_token);
       */
       spotify.token = data.access_token
+      
+      res.cookie('spotifyAuth', data.access_token);
       res.redirect('/camera');
     })
     .catch(err => {
@@ -83,6 +56,48 @@ app.get('/auth-callback', (req, res) => {
    res.send("Error: " + req.query.error);     
   }
 });
+
+
+
+app.use(function(req, res, next) {
+  if (req.cookies.spotifyAuth) {
+    next();
+  } else {
+    console.log('redirect -> /auth');
+    res.redirect('/auth');
+  }
+});
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('camera', {});
+});
+
+app.post('/player', upload.single('file'), async function(req, res) {
+  let imagePath = '/images/' + req.file.filename;
+  let apiResponse = await apiChain(imagePath, req, res);
+  // {error: bool, url: url, errorMessage: errorMessage}
+  if (!apiResponse.error) {
+    res.render('player', {
+      embed: spotify.embed[0] + apiResponse.albumId + spotify.embed[1] 
+    });
+  } else {
+    res.send("Error: " + apiResponse.errorMessage);
+  }
+  try {
+    fs.unlinkSync('/app/public' + imagePath);
+  } catch (err) {
+    console.log('error deleting ' + imagePath + ': ' + err);
+  }
+});
+
+app.get('/player', function(req,res) {
+  res.redirect('/camera');
+});
+
+
 
 
 var listener = app.listen(process.env.PORT, function () {
