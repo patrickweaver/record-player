cf9d9e0110d43404d2edf57e65f53be0806852a0
Glitch (record-player) <none>
2018-04-29 22:34:11 +0000

    🌏🌧 Checkpoint ./public/script.js:217543/330 ./views/layout.hbs:217543/161 ./public/style.css:217543/175

--- public/script.js
+++ public/script.js
@@ -2,3 +2,15 @@ function submitCoverForm() {
   document.getElementById('cover-form').submit();
   document.getElementById('spinner').style.display = "block";
 }
+
+
+var isAdvancedUpload = function() {
+  var div = document.createElement('div');
+  return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+}();
+
+var $form = $('.box');
+
+if (isAdvancedUpload) {
+  $form.addClass('has-advanced-upload');
+}
