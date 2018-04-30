1eed0c86287dff0f2efb029d0b0b8fb8663cebdf
Glitch (record-player) <none>
2018-04-30 02:24:09 +0000

    ⛽️🍰 Checkpoint ./public/script.js:217543/1790 ./views/player.hbs:217543/4 ./views/camera.hbs:217543/96

--- public/script.js
+++ public/script.js
@@ -1,7 +1,32 @@
-console.log("TEST");
+function submitCoverFormOnChange() {
+  var input = document.getElementById('album-cover-image');
+  var file = input.files[0];
+  submitCoverForm(file);
+}
 
-function submitCoverForm() {
-  document.getElementById('cover-form').submit();
+
+
+function submitCoverForm(file) {
+  //document.getElementById('cover-form').submit();
+  document.getElementById('cover-form').style.display = "none";
+  document.getElementById('spinner').style.display = "block";
+
+  console.log(file);
+  var formData = new FormData();
+  formData.append('file', file);
+  formData.append('async', true);
+
+  jQuery.ajax({
+    url: 'player',
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    type: 'POST',
+    success: function(data){
+        window.location.replace('player?albumId=' + data.albumId + '&googleVisionGuess=' + data.googleVisionGuess);
+    }
+  });
 }
 
 
@@ -31,26 +56,8 @@ if (isAdvancedUpload) {
     $form.removeClass('is-dragover');
   })
   .on('drop', function(e) {
-    console.log("DROP!");
-    document.getElementById('cover-form').style.display = "none";
-    document.getElementById('spinner').style.display = "block";
     droppedFiles = e.originalEvent.dataTransfer.files;
-    console.log(droppedFiles[0]);
-    var formData = new FormData();
-    formData.append('file', droppedFiles[0]);
-    formData.append('async', true);
-    
-    jQuery.ajax({
-      url: 'player',
-      data: formData,
-      cache: false,
-      contentType: false,
-      processData: false,
-      type: 'POST',
-      success: function(data){
-          window.location.replace('player?albumId=' + data.albumId + '&googleVisionGuess=' + data.googleVisionGuess);
-      }
-    });
+    console.log("DROP!");
+    submitCoverForm(droppedFiles[0]);
   });
-
 }
