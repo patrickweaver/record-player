30ca799355ba71e2e6411632ad3f52193ab2aa3a
Glitch (record-player) <none>
2018-10-25 18:40:02 +0000

    🍱🐝 Checkpoint ./censoredWords.js:217543/254

--- censoredWords.js
+++ censoredWords.js
@@ -14,19 +14,6 @@ const censoredWords = [
   '[german import]'
 ];
 
-
-// This isn't used right now
-// But maybe it's a good idea to ignore years because
-// The google vision API was including release years a lot
-var semiCensoredWords = [
-  
-]
-
-for (var i = 1900; i < 2030; i++) {
-  semiCensoredWords.push(i.toString); 
-}
-
-
 module.exports = {
   censoredWords: censoredWords
 }
