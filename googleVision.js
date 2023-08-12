2246c861519019163f17e0d84589d19bb24ee579
Glitch (record-player) <none>
2023-08-12 18:53:19 +0000

    Glitch 2023-08-12

--- googleVision.js
+++ googleVision.js
@@ -1,5 +1,6 @@
 const gcpApiUrl = 'https://vision.googleapis.com/v1/images:annotate?'
 const GCP_API_KEY = process.env.GCP_API_KEY;
+const GCP_API_KEY_2 = process.env.GCP_API_KEY_2;
 
 const fs = require('fs');
 
@@ -20,9 +21,20 @@ async function getGcpOptions(imageUrl) {
     return;
   }
   
+  // This is a hack to take advantage of free 1000
+  // requests per month per Google Cloud Account
+  function randomizeKey() {
+    if (GCP_API_KEY_2 && (Math.random() > 0.5)) {
+      console.log("API KEY 2 at " + new Date())
+      return GCP_API_KEY_2;
+    }
+    console.log("API KEY 1 at " + new Date())
+    return  GCP_API_KEY;
+  }
+  
   return {
     method: 'POST',
-    uri: gcpApiUrl + 'key=' + GCP_API_KEY,
+    uri: gcpApiUrl + 'key=' + randomizeKey(),
     body: {
       "requests":[
         {
